937bc165018f002d237772fed615db8b135e14a2
raufzer <a_zerkhef@estin.dz>
2024-11-18 22:34:06 +0100

    chore: adjust user controller

--- internal/controllers/user_controllers.go
+++ internal/controllers/user_controllers.go
@@ -3,8 +3,9 @@ package controllers
 import (
 	"educore-api/internal/models"
 	"educore-api/internal/services"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type UserController struct {
@@ -28,25 +29,48 @@ func (uc *UserController) CreateUser(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	}
-	ctx.JSON(http.StatusOK, gin.H{"message": "User created"})
+	ctx.JSON(http.StatusCreated, gin.H{"message": "User created"})
 }
 
 func (uc *UserController) GetUser(ctx *gin.Context) {
-	
-	ctx.JSON(200, gin.H{"message": "User fetched"})
+	username := ctx.Param("name")
+	user, err := uc.UserService.GetUser(&username)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 }
 
 func (uc *UserController) GetAllUsers(ctx *gin.Context) {
-
-	ctx.JSON(200, gin.H{"message": "All users fetched"})
+	users, err := uc.UserService.GetAllUsers()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, users) // Return users directly
 }
 
 func (uc *UserController) UpdateUser(ctx *gin.Context) {
-
-	ctx.JSON(200, gin.H{"message": "User updated"})
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	err := uc.UserService.UpdateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "User updated"})
 }
 
 func (uc *UserController) DeleteUser(ctx *gin.Context) {
-
-	ctx.JSON(200, gin.H{"message": "User deleted"})
+	username := ctx.Param("name")
+	err := uc.UserService.DeleteUser(&username)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "User deleted"})
 }
