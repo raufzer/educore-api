f2b3c0a8c415a0e2d20882e264226cb0edb8db37
raufzer <a_zerkhef@estin.dz>
2024-11-20 21:25:20 +0100

    chore: config adjust

--- cmd/main.go
+++ cmd/main.go
@@ -5,30 +5,29 @@ import (
 	"educore-api/internal/controllers"
 	"educore-api/internal/repositories"
 	"educore-api/internal/services"
+	"educore-api/routes/api/v1"
+
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Load configuration
+
 	appConfig := config.LoadConfig()
 
-	// Set up the database
 	dbConfig := config.ConnectDatabase(appConfig)
 	defer dbConfig.Client.Disconnect(dbConfig.Ctx)
 
-	// Initialize dependencies
-	userRepo := repositories.NewUserRepository(dbConfig.UserCollection, dbConfig.Ctx)
+	userRepo := repositories.NewUserRepository(dbConfig.UserCollection)
 	userService := services.NewUserService(userRepo)
 	userController := controllers.NewUserController(userService)
 
-	// Set up the Gin server
 	server := gin.Default()
 	basePath := server.Group("/v1")
-	userController.RegisterUserRoutes(basePath)
 
-	// Start the server
+	v1.RegisterUserRoutes(basePath, userController)
+
 	log.Printf("Server running on port %s", appConfig.ServerPort)
 	log.Fatal(server.Run(":" + appConfig.ServerPort))
 }
