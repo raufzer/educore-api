e0802921bb56c9cb102c9dc65bc593bdeba43eaa
raufzer <a_zerkhef@estin.dz>
2024-11-22 17:03:40 +0100

    chore: remove unuesed models

--- internal/models/question.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Question struct {
-	Text       string   `bson:"text" json:"text"`
-	Options    []string `bson:"options" json:"options"`
-	CorrectAns int      `bson:"correct_ans" json:"correctAns"`
-}
